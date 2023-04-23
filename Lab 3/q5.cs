550bae497b75735333e95dd82b21af6f4680cbea
Durgashankar-Bhandari <118617391+Durgashankar-Bhandari@users.noreply.github.com>
2023-04-23 16:05:02 +0000

    Create q5.cs

---
+++ Lab 3/q5.cs
@@ -0,0 +1,40 @@
+﻿/*Write a program in C# Sharp to find the string which starts and ends with a specific character.
+The cities are :
+'ROME','LONDON','NAIROBI','CALIFORNIA','ZURICH','NEW DELHI','AMSTERDAM','ABU DHABI','PARIS'*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StartEndExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>
+            {
+                "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
+                "NEWDELHI", "AMSTERDAM", "ABU DHABI", "PARIS"
+            };
+
+            Console.WriteLine("Enter the starting character:");
+            char startChar = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nEnter the ending character:");
+            char endChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            var result = from city in cities
+                where city.StartsWith(startChar.ToString(), StringComparison.OrdinalIgnoreCase)
+                      && city.EndsWith(endChar.ToString(), StringComparison.OrdinalIgnoreCase)
+                select city;
+
+            Console.WriteLine($"\nCities starting with '{startChar}' and ending with '{endChar}':");
+            foreach (string city in result)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
