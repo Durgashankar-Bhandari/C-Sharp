7c521c00e26833062c895be2c93f8132f49c1f73
Durgashankar-Bhandari <118617391+Durgashankar-Bhandari@users.noreply.github.com>
2023-04-23 16:04:25 +0000

    Create q4.cs

---
+++ Lab 3/q4.cs
@@ -0,0 +1,39 @@
+﻿//Write a program in C# Sharp to display the characters and frequency of character from the string given by the user.
+using System;
+using System.Collections.Generic;
+
+namespace CharacterFrequencyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string inputString = Console.ReadLine();
+            var charFrequency = GetCharacterFrequency(inputString);
+            Console.WriteLine("Character\tFrequency");
+            foreach (var pair in charFrequency)
+            {
+                Console.WriteLine(pair.Key + "\t\t" + pair.Value);
+            }
+            Console.ReadLine();
+        }
+
+        static Dictionary<char, int> GetCharacterFrequency(string inputString)
+        {
+            var charFrequency = new Dictionary<char, int>();
+            foreach (char c in inputString)
+            {
+                if (charFrequency.ContainsKey(c))
+                {
+                    charFrequency[c]++;
+                }
+                else
+                {
+                    charFrequency[c] = 1;
+                }
+            }
+            return charFrequency;
+        }
+    }
+}
