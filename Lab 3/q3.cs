78ba1711d28b2b96a8f6dbc39b6b185abb35c210
Durgashankar-Bhandari <118617391+Durgashankar-Bhandari@users.noreply.github.com>
2023-04-23 16:03:56 +0000

    Create q3.cs

---
+++ Lab 3/q3.cs
@@ -0,0 +1,27 @@
+﻿//Write a program in C# Sharp to find the number of an array and the square of each number using LINQ
+using System;
+using System.Linq;
+
+namespace ArraySquareExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers separated by spaces:");
+            string input = Console.ReadLine();
+            int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+            var result = from num in numbers
+                select new { Number = num, Square = num * num };
+
+            
+            foreach (var data in result)
+            {
+                Console.WriteLine($"The square of {data.Number} is {data.Square}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
