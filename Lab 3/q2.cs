9243f2bda8865ee892edec41cdb830ff4944401e
Durgashankar-Bhandari <118617391+Durgashankar-Bhandari@users.noreply.github.com>
2023-04-23 16:03:14 +0000

    Create q2.cs

---
+++ Lab 3/q2.cs
@@ -0,0 +1,32 @@
+﻿//Write a program to find the upper case letter in string using LINQ
+
+using System;
+using System.Linq;
+
+namespace UpperCaseLettersExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String:");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Original String :"+inputString);
+            var upperCaseLetters = FindUpperCaseLetters(inputString);
+            Console.WriteLine("Uppercase letters in the string:");
+            foreach (var letter in upperCaseLetters)
+            {
+                Console.WriteLine(letter);
+            }
+            
+        }
+
+        static string FindUpperCaseLetters(string inputString)
+        {
+            var upperCaseLetters = from c in inputString
+                where char.IsUpper(c)
+                select c;
+            return new string(upperCaseLetters.ToArray());
+        }
+    }
+}
