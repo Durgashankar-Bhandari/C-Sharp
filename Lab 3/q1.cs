0cb22becb4a38b6a0c5e39ee5fc92f13c30c4891
Durgashankar-Bhandari <118617391+Durgashankar-Bhandari@users.noreply.github.com>
2023-04-23 16:02:27 +0000

    Create q1.cs

---
+++ Lab 3/q1.cs
@@ -0,0 +1,24 @@
+﻿//Write a C# program using async and wait  for long process.
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting long process...");
+            await LongProcessAsync(); 
+            Console.WriteLine("Long process completed.");
+            Console.ReadLine();
+        }
+
+        static async Task LongProcessAsync()
+        {
+            Console.WriteLine("Long process started.");
+            await Task.Delay(5000);
+            Console.WriteLine("Long process completed.");
+        }
+    }
+}
